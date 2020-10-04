cea4a1a30d64712689aecb51be23042c5d62fd62
Janinson Hurtado <yaninhurco@hotmail.com>
2020-10-03 19:07:40 -0500

    se agregaron iconos y un login

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-app-bar 
+    dense
+    color = "#343a40">
+      
+    <v-toolbar-title id = "title">AGRO</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn v-for="item in items_menu" :key="item.id" :to="item.route" style="margin:3px;">
+      <v-list-item-icon>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-icon> 
+    </v-btn>
+
+    </v-app-bar>
+
+    <v-main>
+      <v-container> <nuxt /> </v-container>
+    </v-main>
+
+  </div>
+</template>
+
+<style>
+#title{
+  color: white;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      items_menu: [
+        { id: "01", title: "home", icon: "mdi-cart", route: "/home" },
+        { id: "02", title: "login", icon: "mdi-login-variant", route: "/login" },
+      ],
+    };
+  },
+  methods: {
+    logout() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
---
+++ pages/home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Inicio</h1>
+
+    <p>Aqui se pondra la informacion general del sitio</p>
+  </div>
+</template>
